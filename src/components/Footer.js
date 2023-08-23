d4cdccc9c179c8c2482ca3513fae62ac2bb0bf44
robin-everaert <r-everaert@outlook.fr>
2023-08-23 10:00:18 +0200

    changement Everaert -> Bailly

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <footer id="main-footer" className="d-flex flex-column justify-content-center align-items-center">
             <img src={ logo } alt="Le logo de l'écuries de l'Ajot" className="img img-fluid logo" />
-            <p className="mt-3 text-center">© Marie Everaert - Ecuries de l'Ajot | Tous droits réservés</p>
+            <p className="mt-3 text-center">© Marie Bailly - Ecuries de l'Ajot | Tous droits réservés</p>
             <p className="text-center">3 chemin de l'Ajot 51340 Pargny-sur-Saulx</p>
             <a href="https://www.linkedin.com/in/robin-everaert/?originalSubdomain=fr" className="text-center d-flex align-items-center mt-4" target="_blank">
                 <i className="fa-solid fa-code"></i><span>Site développé par Everaert Robin</span><i className="fa-solid fa-code"></i>
